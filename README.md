55674cdeb5ddb1b81b037b9f23bc814ab763ff24
jomobrain1 <jomobrain1@gmail.com>
2023-02-07 10:12:10 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue
+
+### vs code extensions for vue
+    <ol>
+     Vue 3 snipets by hollowtree
+     Vue language features by vue
+    </ol>
+### Installing latest vue code using vite
+    npm init vue@latest
+
+### Counter app with options api
---
+++ counter-app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const count=ref(0)
+</script>
+
+<template> 
+
+  <main>
+     <h1>Counter Application</h1>
+     <h2>{{count}}</h2>
+     <button @click="count++">increment</button>
+     <button @click="count--">decrement</button>
+  </main>
+</template>
+
+<style scope>
+ 
+</style>
+
+
